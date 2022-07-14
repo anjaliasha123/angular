5723b7bdd771a7f9110af3b6b824843c0ca6a912
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2022-07-14 18:04:10 -0500

    ngOndestroy

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { setInterval } from "timers";
 
 @Component({
   selector: "app-child",
   templateUrl: "./child.component.html"
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnDestroy {
+  counter: number = 0;
+  interval: any;
   constructor() {
     console.log("Child Constructor");
   }
   ngOnInit(): void {
     console.log("Child ngOnInit()");
+    this.interval = setInterval(() => {
+      this.counter = this.counter + 1;
+      console.log(this.counter);
+    }, 1000);
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+    console.log("Child is destroyed");
   }
 }
